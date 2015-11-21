05d23d7cf5387c3699a3a476f175d89f49dcc7ac
Ian Macalinao <me@ian.pw>
2015-11-21 16:02:14 -0600

    Move bootstrap into server dir

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir server
-mkdir server/plugins
-curl http://tcpr.ca/files/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar > server/spigot.jar
---
+++ server/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+mkdir plugins
+curl http://tcpr.ca/files/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar > spigot.jar
+cp ../target/scala-2.11/SysAdmincraft.jar .
