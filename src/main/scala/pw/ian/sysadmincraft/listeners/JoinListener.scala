1b1a179a3e6452d69f40818c7cde96dc4c781d47
Ian Macalinao <me@ian.pw>
2015-11-22 01:46:52 -0600

    add sword to inv

--- src/main/scala/pw/ian/sysadmincraft/listeners/JoinListener.scala
+++ src/main/scala/pw/ian/sysadmincraft/listeners/JoinListener.scala
@@ -2,7 +2,8 @@ package pw.ian.sysadmincraft.listeners
 
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.{EventHandler, Listener}
-import org.bukkit.{GameMode, Location}
+import org.bukkit.inventory.ItemStack
+import org.bukkit.{Material, GameMode, Location}
 import pw.ian.sysadmincraft.SysAdmincraft
 import pw.ian.sysadmincraft.world.WorldConstants._
 
@@ -12,6 +13,7 @@ case class JoinListener(plugin: SysAdmincraft) extends Listener {
   def onPlayerJoin(event: PlayerJoinEvent): Unit = {
     event.getPlayer.setGameMode(GameMode.CREATIVE)
     event.getPlayer.teleport(new Location(plugin.world, 0, START_HEIGHT + 1, 0))
+    event.getPlayer.getInventory.addItem(new ItemStack(Material.IRON_SWORD, 1))
   }
 
 }
