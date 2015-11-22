944f7f81595f1bdd8cff2f73ee3b9b5111899cf3
Ian Macalinao <me@ian.pw>
2015-11-22 11:06:40 -0600

    More info on process

--- src/main/scala/pw/ian/sysadmincraft/system/ProcessAdmin.scala
+++ src/main/scala/pw/ian/sysadmincraft/system/ProcessAdmin.scala
@@ -24,19 +24,29 @@ object ProcessAdmin {
     }.toList
   }
 
-  private def findUserProcesses(): Map[String, (Set[Int], Long, Long)] = {
-    val rawProcessOutput = "ps axco user,pid,rss,vsz,command" !!
+  private def findUserProcesses(): Map[String, (Set[Int], Long, Long, Double, Double, String, String)] = {
+    val rawProcessOutput = "ps axco user,pid,rss,vsz,%cpu,%mem,stat,time,command" !!
 
     rawProcessOutput.split('\n').tail.map(_.split(' ').filter(!_.isEmpty))
       .filter(!_(0).startsWith("root"))
       .filter(!_(0).startsWith("_"))
       .map(_.tail)
-      .map(x => x(3) -> Array(x(0), x(1), x(2)))
-      .groupBy(_._1)
-      .mapValues(_.map(_._2))
-      .mapValues(_.map(x => (x(0).toInt, x(1).toLong, x(2).toLong / 1024)))
+      .map(x => x.slice(7, x.length).mkString(" ") -> (
+        x(0).toInt, // pid
+        x(1).toLong, // rss
+        x(2).toLong / 1024, // vsz
+        x(3).toDouble, // cpu
+        x(4).toDouble, // mem
+        x(5), // stat
+        x(6) // time
+      ))
+      .groupBy(_._1) // Group by process name
+      .mapValues(_.map(_._2)) //
       .mapValues {
-        _.foldLeft((Set[Int](), 0L, 0L)) { (acc, v) => (acc._1 + v._1, acc._2 + v._2, acc._3 + v._3) }
+        _.foldLeft((Set[Int](), 0L, 0L, 0d, 0d, "", "")) { (acc, v) =>
+          (acc._1 + v._1, acc._2 + v._2, acc._3 + v._3,
+          acc._4 + v._4, acc._5 + v._5, v._6, v._7)
+        }
       }
   }
 
